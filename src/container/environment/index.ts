8824b2bf398658eefd4093aebb5a165ed59e4f9b
proyectos <72107627+CelulaBandaAncha@users.noreply.github.com>
2022-11-24 15:31:14 -0500

    update

--- src/container/environment/index.ts
+++ src/container/environment/index.ts
@@ -1,19 +1,11 @@
 
 export const Environment = {
-  /**
-   * Define a quantidade de linhas a ser carregada por padrão nas listagens
-   */
+
   LIMITELINEAS: 5,
-  /**
-   * Placeholder exibido nas inputs
-   */
+
   INPUTBUSCAR: 'Búsqueda...',
-  /**
-   * Texto exibido quando nenhum registro é encontrado em uma listagem
-   */
+  
   LISTAVACIA:'Ningun registro encontrado',
-  /**
-   * Url base de consultado dos dados dessa aplicação
-   */
+ 
   URL_BASE: 'http://localhost:8080/api',
 };
